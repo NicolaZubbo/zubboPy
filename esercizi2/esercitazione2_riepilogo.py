48044407ca9b7d374c61958cd2dee796baf8dbe6
NicolaZubbo <74542345+NicolaZubbo@users.noreply.github.com>
2021-03-14 22:04:12 +0100

    Create esercitazione2_riepilogo.py

---
+++ esercizi2/esercitazione2_riepilogo.py
@@ -0,0 +1,35 @@
+#ESERCIZIO 1
+#numero1 = input("inserisci un numero: ")
+#numero2 = input("inserisci un altro numero: ")
+#numero3 = input("inserisci un altro numero ancora: ")
+#numero4 = input("inserisci ancora un altro numero: ")
+#numero5 = input("inserisci un ultimo numero: ")
+#array=[numero1, numero2, numero3, numero4, numero5]
+#print("la lista è: ", array)
+#for i in reversed(array):
+#    print(i)
+
+
+#ESERCIZIO 3
+#import numpy as np
+#lista = np.array([[1,2,3],[4,5,6],[7,8,9]])
+#print(lista)
+#scelta_numero = int(input('quale colonna vorresti visualizzare: '))
+#print([row[scelta_numero] for row in lista])
+
+
+#ESERCIZIO 4
+#from random import randint
+#f = open("testo.txt", "w")
+#
+#for riga in range(100):
+#
+#    for elemento in range(1):
+#
+#        testo = testo + str(randint(1,100)) + "," + str(randint(1,100))
+#
+#   testo = testo + "\n"
+#
+#print(testo)
+#f.write(testo)
+#f.close()
