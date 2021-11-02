39602d2f6e78a9a43e327612ebbf7a2f52be2054
NicolaZubbo <74542345+NicolaZubbo@users.noreply.github.com>
2021-11-02 21:41:25 +0100

    Update combinazioni_prova.py

--- oop/combinazioni_prova.py
+++ oop/combinazioni_prova.py
@@ -2,9 +2,10 @@ strin = "cane"
 
 n = len(strin)
 
-while nuova_n>=2:
+def permutazione(strin): 
+  while nuova_n>=2:
     nuova_n = n-1
     permut = n * nuova_n
-    
+    return permut
 
 print(permut)
